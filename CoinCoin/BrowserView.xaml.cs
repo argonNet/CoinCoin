ca8c341c3cb2c8eb73868ce23d3fd8f31743c1d8
Sébastien Schoepfer <sebastien.schoepfer@frontiersin.net>
2025-02-01 21:49:38 +0100

    feature: adding tabs

---
+++ CoinCoin/BrowserView.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+using Microsoft.Web.WebView2.Core;
+
+namespace CoinCoin;
+
+public partial class BrowserView : UserControl
+{
+    public BrowserView()
+    {
+        InitializeComponent();
+        InitializeWebView();
+    }
+    
+    private string _currentUrl;
+    public string CurrentUrl
+    {
+        get => _currentUrl;
+        set
+        {
+            if (_currentUrl != value)
+            {
+                _currentUrl = value;
+                webView.Source = new Uri(_currentUrl);
+                urlTextBox.Text = CurrentUrl;
+            }
+        }
+    }
+    
+    async void InitializeWebView()
+    {
+        await webView.EnsureCoreWebView2Async(null);
+        webView.NavigationCompleted += WebView_NavigationCompleted;
+    }
+
+    private void WebView_NavigationCompleted(object sender, CoreWebView2NavigationCompletedEventArgs e)
+    {
+        if (webView.Source != null)
+        {
+            CurrentUrl = webView.Source.ToString();
+        }
+    }
+
+    private void goToButton_Click(object sender, RoutedEventArgs e)
+    {
+        CurrentUrl = urlTextBox.Text;
+    }
+}
--- CoinCoin/MainWindow.xaml.cs
+++ CoinCoin/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Ribbon;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -13,54 +14,31 @@ using Microsoft.Web.WebView2.Core;
 
 namespace CoinCoin;
 
-/// <summary>
-/// Interaction logic for MainWindow.xaml
-/// </summary>
 public partial class MainWindow : Window
 {
-    private string _currentUrl;
-    public string CurrentUrl
-    {
-        get => _currentUrl;
-        set
-        {
-            if (_currentUrl != value)
-            {
-                _currentUrl = value;
-                OnPropertyChanged(nameof(CurrentUrl));
-                webView.Source = new Uri(_currentUrl);
-            }
-        }
-    }
-    
     public MainWindow()
     {
         InitializeComponent();
     }
     
-    async void InitializeWebView()
-    {
-        await webView.EnsureCoreWebView2Async(null);
-        webView.NavigationCompleted += WebView_NavigationCompleted;
-        CurrentUrl = webView.Source.ToString();
-    }
-
-    private void WebView_NavigationCompleted(object sender, CoreWebView2NavigationCompletedEventArgs e)
-    {
-        if (webView.Source != null)
+    private void AddTab_Click(object sender, RoutedEventArgs e)
+    { 
+        var newTab = new TabItem
         {
-            CurrentUrl = webView.Source.ToString();
+            Header = $"Tab",
+            HeaderTemplate = (DataTemplate)TabControl.Resources["TabHeaderTemplate"]!,
+            Content = new BrowserView()
+        };
+        
+        TabControl.Items.Add(newTab);
+        newTab.IsSelected = true;
+    }
+    
+    private void CloseTab_Click(object sender, RoutedEventArgs e)
+    {
+        if (sender is Button button && button.Tag is TabItem tabItem)
+        {
+            TabControl.Items.Remove(tabItem);
         }
-    }
-
-    private void goToButton_Click(object sender, RoutedEventArgs e)
-    {
-        CurrentUrl = urlTextBox.Text;
-    }
-
-    public event PropertyChangedEventHandler PropertyChanged;
-    protected void OnPropertyChanged(string propertyName)
-    {
-        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
 }
